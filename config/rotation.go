b936f96f17a663305ef465785a169255f2169281
Lunar Chipter <lunarchipter@gmail.com>
2025-11-27 20:32:27 +0800

    Add files via upload

--- config/rotation.go
+++ config/rotation.go
@@ -1,1 +1,17 @@
+package config
 
+import (
+	"time"
+)
+
+// RotationConfig holds configuration for log rotation
+// RotationConfig menyimpan konfigurasi untuk rotasi log
+type RotationConfig struct {
+	MaxSize         int64
+	MaxAge          time.Duration
+	MaxBackups      int
+	LocalTime       bool
+	Compress        bool
+	RotationTime    time.Duration
+	FilenamePattern string
+}
